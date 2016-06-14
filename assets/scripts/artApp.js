95b3f844ab50250c6d125828c08ec9fd9b33cdca
Kei Sato <ckeisato@gmail.com>
2016-06-13 22:04:28 -0400

    Fixes

--- assets/scripts/artApp.js
+++ assets/scripts/artApp.js
@@ -4,8 +4,6 @@ var artPage = {
 	modalID : 'artModal',
 
 	init: function(){
-		$(document).foundation();
-
 		this.$grid = document.querySelector(this.gridID);
 
 		that = this;
@@ -21,17 +19,6 @@ var artPage = {
 		};
   },
 
-	// setModals: function(){
-	// 	var that = this;
-	//
-	// 	$('[data-reveal-id]').on('click', function() {
-	// 	  var key = this.getAttribute("data-imgkey");
-	// 	  $('#' + that.modalID + ' img').attr('src', './assets/' + that.jsondata[key].filename);
-	// 	  $('#' + that.modalID + ' #modal-caption').text(that.jsondata[key].desc);
-	// 	  $('#' + that.modalID + ' #modal-title').text(that.jsondata[key].title);
-	// 	});
-	// },
-
 	showImages: function(){
 		var that = this;
 		var gridImages = document.getElementsByClassName("js-grid-img");
@@ -45,25 +32,24 @@ var artPage = {
 	},
 
 	setImageElements (data){
+
 		for (var key in data){
 			 var filename = data[key].filename;
 
 			 var listItem = document.createElement("li");
-			 listItem.classList.add("column");
-
-			 var listItemLink = document.createElement("button");
-			 listItemLink.href = "#";
-			 listItemLink.setAttribute("data-reveal-id", that.modalID);
-			 listItemLink.setAttribute("data-imgkey", key);
+			 listItem.classList.add("image-grid-item");
 
 			 var listItemImg = document.createElement("img");
 			 listItemImg.setAttribute("src", "./assets/" + filename);
 			 listItemImg.classList.add("js-grid-img");
 			 listItemImg.classList.add("opacity-transition");
 
-			 listItemLink.appendChild(listItemImg);
-			 listItem.appendChild(listItemLink);
-			 that.$grid.appendChild(listItem);
+			 listItem.appendChild(listItemImg);
+			 this.$grid.appendChild(listItem);
+
+			 this.msnry.reloadItems();
+			 console.log(this.msnry.reloadItems);
+
 		 }
 	},
 
@@ -80,7 +66,6 @@ var artPage = {
 				that.jsondata = data;
 				that.setImageElements(data);
 				that.showImages();
-				that.setModals();
 		  } else {
 				alert("There's been an issue with the images, try refreshing the page");
 		  }
